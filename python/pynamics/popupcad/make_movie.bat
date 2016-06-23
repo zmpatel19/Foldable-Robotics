375e35a0922e6c58dccb5bc6e11bc3ab007b7d4d
Dan Aukes <danaukes@asu.edu>
2016-06-23 10:38:32 -0700

    removed popupcad code

--- python/pynamics/popupcad/make_movie.bat
+++
@@ -1,2 +0,0 @@
-"C:/program files/ffmpeg/bin/ffmpeg" -r 30 -i render/%%04d.png -vcodec libxvid render.mp4
-REM "C:/program files/ffmpeg/bin/ffmpeg" -r 30 -i render/%%04d.png -vcodec libx264 render.mp4
