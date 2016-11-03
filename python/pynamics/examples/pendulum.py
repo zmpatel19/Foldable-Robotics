2cfb369e45488b385966ebf060d4f96e9c8ed55f
Dan Aukes <danaukes@gmail.com>
2016-11-02 22:15:33 -0700

    updated

---
+++ python/pynamics/examples/pendulum.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+"""
+Written by Daniel M. Aukes
+Email: danaukes<at>gmail.com
+Please see LICENSE for full license.
+"""
+
+import pynamics
+#pynamics.script_mode = True
+from pynamics.frame import Frame
+from pynamics.variable_types import Differentiable,Constant
+from pynamics.system import System
+from pynamics.body import Body
+from pynamics.dyadic import Dyadic
+from pynamics.output import Output
+from pynamics.particle import Particle
+
+#import sympy
+import numpy
+import scipy.integrate
+import matplotlib.pyplot as plt
+plt.ion()
+from sympy import pi
+system = System()
+
+lA = Constant('lA',1,system)
+
+mA = Constant('mA',1,system)
+
+g = Constant('g',9.81,system)
+b = Constant('b',1e0,system)
+k = Constant('k',1e1,system)
+
+tinitial = 0
+tfinal = 5
+tstep = .001
+t = numpy.r_[tinitial:tfinal:tstep]
+
+preload1 = Constant('preload1',0*pi/180,system)
+
+qA,qA_d,qA_dd = Differentiable(system,'qA')
+
+initialvalues = {}
+initialvalues[qA]=0*pi/180
+initialvalues[qA_d]=0*pi/180
+
+statevariables = system.get_q(0)+system.get_q(1)
+ini = [initialvalues[item] for item in statevariables]
+
+N = Frame('N')
+A = Frame('A')
+
+system.set_newtonian(N)
+A.rotate_fixed_axis_directed(N,[0,0,1],qA,system)
+
+pNA=0*N.x
+pAB=pNA+lA*A.x
+vAB=pAB.time_derivative(N,system)
+
+ParticleA = Particle(system,pAB,mA,'ParticleA')
+
+system.addforce(-b*vAB,vAB)
+
+system.addforcegravity(-g*N.y)
+
+x1 = ParticleA.pCM.dot(N.x)
+y1 = ParticleA.pCM.dot(N.y)
+KE = system.KE
+PE = system.getPEGravity(pNA) - system.getPESprings()
+    
+pynamics.tic()
+print('solving dynamics...')
+f,ma = system.getdynamics()
+print('creating second order function...')
+func1 = system.createsecondorderfunction2(f,ma)
+print('integrating...')
+states=scipy.integrate.odeint(func1,ini,t,rtol=1e-12,atol=1e-12,hmin=1e-14)
+pynamics.toc()
+print('calculating outputs..')
+output = Output([x1,y1,KE-PE,qA],system)
+y = output.calc(states)
+pynamics.toc()
+
+plt.figure(1)
+plt.hold(True)
+plt.plot(y[:,0],y[:,1])
+plt.axis('equal')
+
+plt.figure(2)
+plt.plot(y[:,2])
+
+plt.figure(3)
+plt.hold(True)
+plt.plot(t,y[:,3])
+plt.show()
---
+++ python/pynamics/examples/pendulum_mod.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+"""
+Written by Daniel M. Aukes
+Email: danaukes<at>gmail.com
+Please see LICENSE for full license.
+"""
+
+import pynamics
+#pynamics.script_mode = True
+from pynamics.frame import Frame
+from pynamics.variable_types import Differentiable,Constant,Variable
+from pynamics.system import System
+from pynamics.body import Body
+from pynamics.dyadic import Dyadic
+from pynamics.output import Output
+from pynamics.particle import Particle
+
+import sympy
+import numpy
+import scipy.integrate
+import matplotlib.pyplot as plt
+plt.ion()
+from sympy import pi
+system = System()
+
+lA = Constant('lA',1,system)
+
+mA = Constant('mA',1,system)
+
+g = Constant('g',9.81,system)
+b = Constant('b',1e0,system)
+k = Constant('k',1e1,system)
+
+tinitial = 0
+tfinal = 5
+tstep = .001
+t = numpy.r_[tinitial:tfinal:tstep]
+
+preload1 = Constant('preload1',0*pi/180,system)
+
+x,x_d,x_dd = Differentiable(system,'x')
+y,y_d,y_dd = Differentiable(system,'y')
+
+initialvalues = {}
+initialvalues[x]=1
+initialvalues[x_d]=0
+initialvalues[y]=0
+initialvalues[y_d]=0
+
+statevariables = system.get_q(0)+system.get_q(1)
+ini = [initialvalues[item] for item in statevariables]
+
+N = Frame('N')
+
+system.set_newtonian(N)
+
+pNA=0*N.x
+pAB=pNA+x*N.x+y*N.y
+vAB=pAB.time_derivative(N,system)
+
+ParticleA = Particle(system,pAB,mA,'ParticleA')
+
+system.addforce(-b*vAB,vAB)
+
+system.addforcegravity(-g*N.y)
+
+x1 = ParticleA.pCM.dot(N.x)
+y1 = ParticleA.pCM.dot(N.y)
+KE = system.KE
+PE = system.getPEGravity(pNA) - system.getPESprings()
+
+v = pAB-pNA
+eq = [(v.dot(v))**.5 - lA]
+J = sympy.Matrix(eq).jacobian(system.get_q(0))
+
+def createsecondorderfunction2(system,f,ma):
+    q_state = system.get_q(0)+system.get_q(1)
+    q_state_d = system.get_q(1) + system.get_q(2)
+    f = sympy.Matrix(f)
+    ma = sympy.Matrix(ma)
+    q = system.get_q(0)
+    q_d = system.get_q(1)
+    q_dd = system.get_q(2)
+
+    Ax_b = ma-f
+    x = sympy.Matrix(q_dd)
+    A = Ax_b.jacobian(x)
+    b = -Ax_b.subs(dict(list([(item,0) for item in x])))
+    
+    m = len(q_d)
+    A_full = sympy.zeros(2*m)   
+    A_full[:m,:m] = sympy.eye(m)
+    A_full[m:,m:] = A
+    b_full = sympy.zeros(2*m,1)
+    b_full[:m,0]=q_d
+    b_full[m:,0]=b
+    
+    c_sym = list(system.constants.keys())
+    c_val = [system.constants[key] for key in c_sym]
+
+    fA = sympy.lambdify(q_state+c_sym,A_full)
+    fb = sympy.lambdify(q_state+c_sym,b_full)
+
+    def func(state,time):
+        a = list(state)+c_val
+        Ai = fA(*a)
+        bi = fb(*a)
+        x = numpy.array(scipy.linalg.inv(Ai).dot(bi))
+        return x.flatten().tolist()
+    return func
+    
+pynamics.tic()
+print('solving dynamics...')
+f,ma = system.getdynamics()
+print('creating second order function...')
+func1 = createsecondorderfunction2(system,f,ma)
+print('integrating...')
+states=scipy.integrate.odeint(func1,ini,t,rtol=1e-12,atol=1e-12,hmin=1e-14)
+pynamics.toc()
+print('calculating outputs..')
+output = Output([x1,y1,KE-PE,x,y],system)
+y = output.calc(states)
+pynamics.toc()
+
+plt.figure(1)
+plt.hold(True)
+plt.plot(y[:,0],y[:,1])
+plt.axis('equal')
+
+plt.figure(2)
+plt.plot(y[:,2])
+
+plt.figure(3)
+plt.hold(True)
+plt.plot(t,y[:,3])
+plt.show()
