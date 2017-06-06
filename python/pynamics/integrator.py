1961bd7daf4f61fb3a7e946eabb90dae6a63aa97
Dan Aukes <danaukes@gmail.com>
2017-06-06 13:22:17 -0700

    added args to integrator

--- python/pynamics/integrator.py
+++ python/pynamics/integrator.py
@@ -17,24 +17,24 @@ class RK4(object):
         self.h_min = h_min or self.h*1e-3
         self.h_max = self.h
         self.x0 = x0
-    def step(self,t,x):
+    def step(self,t,x,args = None):
         h = self.h
         f = self.f
         
         x = numpy.array(x)
         
-        k1 = numpy.array(f(x,t))
-        k2 = numpy.array(f((x+k1*(h/2)),t+h/2))
-        k3 = numpy.array(f((x+k2*(h/2)),t+h/2))
-        k4 = numpy.array(f((x+k3*h),t+h))        
+        k1 = numpy.array(f(x,t,*args))
+        k2 = numpy.array(f((x+k1*(h/2)),t+h/2,*args))
+        k3 = numpy.array(f((x+k2*(h/2)),t+h/2,*args))
+        k4 = numpy.array(f((x+k3*h),t+h,*args))        
         
         x1 = x+(k1+2*k2+2*k3+k4)/6*h
     
         return x1
     
-    def run(self):
+    def run(self,args = None):
         x = []
         x.append(self.x0)
         for time in self.t:
-            x.append(self.step(time,x[-1]))
+            x.append(self.step(time,x[-1],args = args))
         return numpy.array(x)[1:]
