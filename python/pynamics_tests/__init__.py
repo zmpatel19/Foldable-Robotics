e7cdbad9d7d6ee122ce83d7467fbb86127a0f25f
Dan Aukes <danaukes@gmail.com>
2017-06-06 12:42:34 -0700

    moved examples to their own structure for now

---
+++ python/pynamics_tests/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  6 12:29:54 2017
+
+@author: danaukes
+"""
+import pynamics
+pynamics.script_mode = False
+import pynamics_examples.babyboot
+import pynamics_examples.ball_rope
+#import pynamics_examples.birdleg
+import pynamics_examples.bouncy
+import pynamics_examples.bouncy2
+import pynamics_examples.differentiating
+import pynamics_examples.double_pendulum_lagrange
+import pynamics_examples.five_bar
+import pynamics_examples.gyro
+import pynamics_examples.pendulum
+import pynamics_examples.pendulum_inertia
+import pynamics_examples.triple_pendulu
+import pynamics_examples.triple_pendulum_inertia
+import pynamics_examples.triple_pendulum_constrained
+import pynamics_examples.triple_pendulum_global
+
