72eb5b3db861f50d47af92cd4b4528a9d5dbae94
Dan Aukes <danaukes@gmail.com>
2017-10-09 15:45:45 -0700

    added comments to five bar

--- python/pynamics_examples/five_bar.py
+++ python/pynamics_examples/five_bar.py
@@ -34,6 +34,8 @@ from math import pi,sin,cos
 #lB = Constant('lB',1,system)
 #lC = Constant('lC',1,system)
 
+################################################
+#This is where we set the link angles
 
 t1 = 50*pi/180
 t2 = 70*pi/180
@@ -41,29 +43,41 @@ t3 = 70*pi/180
 t4 = 60*pi/180
 t0 = 2*pi-(t1+t2+t3+t4)
 
+################################################
+#mass of the system
 m = Constant(1,'m',system)
 
+################################################
+#Define constant data types, seeded with constant values
 t0 = Constant(t0,'t0',system)
 t1 = Constant(t1,'t1',system)
 t2 = Constant(t2,'t2',system)
 t3 = Constant(t3,'t3',system)
 t4 = Constant(t4,'t4',system)
 
+################################################
+#other constants
 g = Constant(0,'g',system)
 b = Constant(1e0,'b',system)
 k = Constant(1e2,'k',system)
 
+################################################
+#time parameters
 tinitial = 0
 tfinal = 5
 tstep = .05
 t = numpy.r_[tinitial:tfinal:tstep]
 
+################################################
+#State variables
 qA1,qA1_d,qA1_dd = Differentiable('qA1',system)
 qA2,qA2_d,qA2_dd = Differentiable('qA2',system)
 qA3,qA3_d,qA3_dd = Differentiable('qA3',system)
 qB1,qB1_d,qB1_dd = Differentiable('qB1',system)
 qB2,qB2_d,qB2_dd = Differentiable('qB2',system)
 
+################################################
+#Define initial values for state variables
 initialvalues = {}
 initialvalues[qA1]=.1*pi/180
 initialvalues[qA2]=.1*pi/180
@@ -80,6 +94,8 @@ initialvalues[qB2_d]=0*pi/180
 statevariables = system.get_state_variables()
 ini = [initialvalues[item] for item in statevariables]
 
+################################################
+#Create Frames
 N = Frame('N')
 A1 = Frame('A1')
 A12 = Frame('A12')
@@ -94,6 +110,8 @@ B12 = Frame('B12')
 B2 = Frame('B2')
 B23 = Frame('B23')
 
+################################################
+#Relative frame rotations from newtonian out to distal frames
 system.set_newtonian(N)
 
 A1.rotate_fixed_axis_directed(N,[1,0,0],qA1,system)
@@ -109,6 +127,9 @@ B12.rotate_fixed_axis_directed(B1,[0,0,1],-t4,system)
 B2.rotate_fixed_axis_directed(B12,[1,0,0],qB2,system)
 B23.rotate_fixed_axis_directed(B2,[0,0,1],-t3,system)
 
+
+################################################
+#Define particles at the center of mass of each body
 pNO = 0*N.x
 
 ParticleA1 = Particle(A1.x+A12.x,m,'ParticleA1',system)
@@ -117,13 +138,16 @@ ParticleA3 = Particle(A3.x+A34.x,m/2,'ParticleA3',system)
 ParticleB1 = Particle(B1.x+B12.x,m,'ParticleA1',system)
 ParticleB2 = Particle(B2.x+B23.x,m/2,'ParticleA2',system)
 
-
+################################################
+#Get the relative rotational velocity between frames
 wA1 = N.getw_(A1)
 wA2 = A12.getw_(A2)
 wA3 = A23.getw_(A3)
 wB1 = NB1.getw_(B1)
 wB2 = B12.getw_(B2)
 
+################################################
+#Add damping between joints
 system.addforce(-b*wA1,wA1)
 system.addforce(-b*wA2,wA2)
 system.addforce(-b*wA3,wA3)
@@ -132,15 +156,21 @@ system.addforce(-b*wB2,wB2)
 
 #system.addforce(1*A1.x,wA1)
 
+################################################
+#Add spring forces to two joints
 system.add_spring_force(k,(qA1-pi/180*45)*A1.x,wA1) 
 #system.add_spring_force(k,(qA2)*A2.x,wA2) 
 #system.add_spring_force(k,(qA3)*A3.x,wA3) 
 system.add_spring_force(k,(qB1+pi/180*45)*B1.x,wB1) 
 #system.add_spring_force(k,(qB2)*B2.x,wB2) 
 
+################################################
+#Add gravity
 system.addforcegravity(-g*N.z)
 
 
+################################################
+#variables for constraint equation solving
 v1 = B23.x - A3.x
 v2 = B23.y - A3.y
 v3 = B23.z - A3.z
@@ -162,6 +192,7 @@ eq1.append(v3.dot(v3))
 #eq1.append((A34.x-B2.x).dot(N.z))
 #eq1.append((ParticleA3.pCM-ParticleB2.pCM).dot(N.z))
 
+#take the derivative of those equations twice
 eq1_d=[(system.derivative(item)) for item in eq1]
 eq1_dd=[(system.derivative(item)) for item in eq1_d]
 eq = eq1_dd
@@ -173,14 +204,25 @@ eq = eq1_dd
 #y2 = ParticleB.pCM.dot(N.y)
 #x3 = ParticleC.pCM.dot(N.x)
 #y3 = ParticleC.pCM.dot(N.y)
+################################################
+#retrive the energy of the system for plotting purposes
 KE = system.KE
 PE = system.getPEGravity(pNO) - system.getPESprings()
+
     
+################################################
+#solve equations
 pynamics.tic()
 print('solving dynamics...')
+
+################################################
+#This is F and MA
 f,ma = system.getdynamics()
 print('creating second order function...')
-#func1 = system.state_space_post_invert(f,ma,eq)
+#func1 = system.state_space_post_invert(f,ma,eq1_dd)
+
+################################################
+#this is the function you integrate
 func1 = system.state_space_post_invert2(f,ma,eq1_dd,eq1_d,eq1)
 
 print('integrating...')
