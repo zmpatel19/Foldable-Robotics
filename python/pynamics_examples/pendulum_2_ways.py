6d17d6297a94d3e87663a04f9f9cd4547095cb66
Dan Aukes <danaukes@gmail.com>
2017-11-09 22:13:46 -0700

    updated pendulum and example names

--- python/pynamics_examples/pendulum_2_ways.py
+++ python/pynamics_examples/pendulum_2_ways.py
@@ -7,14 +7,14 @@ Please see LICENSE for full license.
 
 import pynamics
 from pynamics.frame import Frame
-from pynamics.variable_types import Differentiable,Constant
+from pynamics.variable_types import Differentiable,Constant,Variable
 from pynamics.system import System
 from pynamics.body import Body
 from pynamics.dyadic import Dyadic
-from pynamics.output import Output
+from pynamics.output import Output,PointsOutput
 from pynamics.particle import Particle
 
-#import sympy
+import sympy
 import numpy
 import scipy.integrate
 import matplotlib.pyplot as plt
@@ -32,16 +32,22 @@ k = Constant(1e1,'k',system)
 
 tinitial = 0
 tfinal = 5
-tstep = .001
+tstep = 1/30
 t = numpy.r_[tinitial:tfinal:tstep]
 
 preload1 = Constant(0*pi/180,'preload1',system)
 
 qA,qA_d,qA_dd = Differentiable('qA',system)
+x,x_d,x_dd = Differentiable('x',system)
+y,y_d,y_dd = Differentiable('y',system)
 
 initialvalues = {}
 initialvalues[qA]=0*pi/180
 initialvalues[qA_d]=0*pi/180
+initialvalues[x]=1
+initialvalues[y]=0
+initialvalues[x_d]=0
+initialvalues[y_d]=0
 
 statevariables = system.get_state_variables()
 ini = [initialvalues[item] for item in statevariables]
@@ -56,44 +62,36 @@ pNA=0*N.x
 pAB=pNA+lA*A.x
 vAB=pAB.time_derivative(N,system)
 
+pNA2=2*N.x
+pAB2=pNA2+x*N.x+y*N.y
+vAB2=pAB2.time_derivative(N,system)
+
 ParticleA = Particle(pAB,mA,'ParticleA',system)
+ParticleB = Particle(pAB2,mA,'ParticleB',system)
 
 system.addforce(-b*vAB,vAB)
+system.addforce(-b*vAB2,vAB2)
 system.addforcegravity(-g*N.y)
 
-x1 = ParticleA.pCM.dot(N.x)
-y1 = ParticleA.pCM.dot(N.y)
 
-    
+v = pAB2-pNA2
+u = (v.dot(v))**.5
+
+eq1 = [(v.dot(v)) - lA**2]
+eq1_dd=system.derivative(system.derivative(eq1[0]))
+eq = [eq1_dd]
+
 pynamics.tic()
 print('solving dynamics...')
 f,ma = system.getdynamics()
 print('creating second order function...')
-func = system.state_space_post_invert(f,ma)
+func = system.state_space_post_invert(f,ma,eq)
 print('integrating...')
-#from pynamics.integrator import RK4,DoPri
-#integrator = RK4(func,ini,t)
-#integrator = DoPri(func,ini,t)
-#states = integrator.run()
 states=scipy.integrate.odeint(func,ini,t,rtol=1e-12,atol=1e-12,hmin=1e-14, args=({'constants':system.constant_values},))
-
 pynamics.toc()
 print('calculating outputs..')
 
-KE = system.get_KE()
-PE = system.getPEGravity(pNA) - system.getPESprings()
-
-output = Output([x1,y1,KE-PE,qA],system)
-y = output.calc(states)
-pynamics.toc()
-
-plt.figure(1)
-plt.plot(y[:,0],y[:,1])
-plt.axis('equal')
-
-plt.figure(2)
-plt.plot(y[:,2])
-
-plt.figure(3)
-plt.plot(t,y[:,0])
-plt.show()
+points = [pNA,pAB,pNA,pNA2,pAB2]
+points_output = PointsOutput(points,system)
+points_output.calc(states)
+points_output.animate(fps = 30)
