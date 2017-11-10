256da684c6a26049e9653b352ab82372e01ee65d
Dan Aukes <danaukes@gmail.com>
2017-11-09 20:08:08 -0700

    new points output

--- python/pynamics/output.py
+++ python/pynamics/output.py
@@ -33,3 +33,23 @@ class Output(object):
             plt.plot(self.y)
         else:
             plt.plot(t,self.y)
+
+#class PointsOutput(Output):
+#    def init(self,y_exp, system=None, constant_values = None):
+#        system = system or pynamics.get_system()
+#        y_exp = [item for item2 in y_exp for item in [item2.dot(system.newtonian.x),item2.dot(system.newtonian.y)]]
+#        super(PointsOutput,self).__init__(y_exp, system, constant_values )
+#
+#    def plot_xy(self):
+#        def plot_time(self,t=None):
+#        import matplotlib.pyplot as plt
+#        plt.figure()
+#        try:
+#            self.y
+#        except AttributeError:
+#            self.calc()
+#        if t is None:
+#            plt.plot(self.y)
+#        else:
+#            plt.plot(t,self.y)
+        
