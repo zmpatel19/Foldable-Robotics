3cff7dd4aa45c632a3ef4616d3af0aef5acb9f7a
Dan Aukes <danaukes@gmail.com>
2017-11-29 14:57:07 -0700

    upgraded to v0.0.4

--- setup.py
+++ setup.py
@@ -14,14 +14,14 @@ shutil.rmtree("dist", ignore_errors=True)
 shutil.rmtree('pynamics.egg-info', ignore_errors=True)
 
 
-packages = [ 'pynamics']
+packages = ['pynamics','pynamics_examples']
 
 package_data = {}
 package_data['pynamics'] = []
 
 setup_kwargs = {}
 setup_kwargs['name']='pynamics'
-setup_kwargs['version']='0.0.2'
+setup_kwargs['version']='0.0.4'
 setup_kwargs['classifiers']=['Programming Language :: Python','Programming Language :: Python :: 3']   
 setup_kwargs['description']='Pynamics is a package for creating and solving dynamic equations of motion symbolically'
 setup_kwargs['author']='Dan Aukes'
@@ -29,7 +29,7 @@ setup_kwargs['author_email']='danaukes@danaukes.com'
 setup_kwargs['url']='https://github.com/idealabasu/code_pynamics'
 setup_kwargs['license']='MIT'
 setup_kwargs['packages']=packages
-setup_kwargs['package_dir']={'pynamics' : 'python/pynamics'}
+setup_kwargs['package_dir']={'pynamics' : 'python/pynamics','pynamics_examples':'python/pynamics_examples'}
 setup_kwargs['package_data'] = package_data
 setup_kwargs['install_requires']=['idealab_tools','matplotlib','numpy','scipy','sympy']
   
