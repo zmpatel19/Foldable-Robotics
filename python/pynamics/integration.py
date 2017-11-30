c44181c06ddd0177a3ddfd675d1703554040223a
Dan Aukes <danaukes@gmail.com>
2017-11-30 08:37:17 -0700

    fixed omission

--- python/pynamics/integration.py
+++ python/pynamics/integration.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 30 08:36:33 2017
+
+@author: daukes
+"""
+
+import numpy
+import logging
+logger = logging.getLogger('pynamics.integration')
+
 def integrate_odeint(*arguments,**keyword_arguments):
     import scipy.integrate
     
