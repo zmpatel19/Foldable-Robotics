3720b7c1a15e459258681888c73d20b6559fed2e
Dan Aukes <danaukes@gmail.com>
2017-12-21 14:13:07 -0700

    working on bayo-ledesma algo

--- python/pynamics/dynamics_solvers.py
+++ python/pynamics/dynamics_solvers.py
@@ -8,11 +8,11 @@ import sympy
 
 def solve_bayo_ledesma(system,f,ma,state,constants = None):
     constants = constants or {}
-    state_d = [system.derivative[item] for item in q]
+    state_d = [system.derivatives[item] for item in state]
     q_d = [item for item in state_d if item in system.get_q(1)]
     q_dd = [item for item in state_d if item in system.get_q(2)]
     Ax_b = sympy.Matrix(f)-sympy.Matrix(ma)
-    Ax_b = A.subs(constants)
+    Ax_b = Ax_b.subs(constants)
     A = Ax_b.jacobian(q_dd)
     b = -((Ax_b).subs(dict([(item,0) for item in q_dd])))
     return A,b
