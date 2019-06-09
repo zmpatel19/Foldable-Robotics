f8be81d082434ba35d4173121ed025eba4ef9fc6
Dan Aukes <danaukes@gmail.com>
2019-06-09 16:59:59 -0700

    new code for pogo stick with wings

--- python/pynamics_examples/body_in_space.py
+++ python/pynamics_examples/body_in_space.py
@@ -12,10 +12,10 @@ from pynamics.system import System
 from pynamics.body import Body
 from pynamics.dyadic import Dyadic
 from pynamics.output import Output,PointsOutput
-from pynamics.particle import Particle
+#from pynamics.particle import Particle
 import pynamics.integration
 pynamics.script_mode = True
-
+import sympy
 #import logging
 #pynamics.logger.setLevel(logging.ERROR)
 #pynamics.system.logger.setLevel(logging.ERROR)
@@ -23,67 +23,85 @@ pynamics.script_mode = True
 import numpy
 import matplotlib.pyplot as plt
 plt.ion()
-from math import pi
+from math import pi, sin, cos
 system = System()
 
-g = Constant(9.81,'g',system)
 
 tinitial = 0
-tfinal = 5
+tfinal = 2.5
 tstep = 1/30
 t = numpy.r_[tinitial:tfinal:tstep]
 
-Differentiable('qA')
-Differentiable('qB')
-Differentiable('qC')
+Differentiable('x',ini=[0,10*cos(45*pi/180)])
+Differentiable('y',ini=[1,10*sin(45*pi/180)])
+#Differentiable('z',ini=[0,0])
 
-Differentiable('x')
-Differentiable('y')
-Differentiable('z')
+#Differentiable('qA',ini=[0,0])
+#Differentiable('qB',ini=[0,0])
+Differentiable('qC',ini=[45*pi/180,0])
 
-Constant(1,'mC')
-Constant(2,'Ixx')
-Constant(3,'Iyy')
-Constant(1,'Izz')
 
-initialvalues = {}
-initialvalues[qA]=0*pi/180
-initialvalues[qA_d]=7
-initialvalues[qB]=0*pi/180
-initialvalues[qB_d]=.2
-initialvalues[qC]=0*pi/180
-initialvalues[qC_d]=.2
+Constant(9.81,'g')
+Constant(.05,'mC')
+Constant(6e-3,'Ixx')
+Constant(6e-3,'Iyy')
+Constant(6e-3,'Izz')
+Constant(.35,'l')
+Constant(1.292,'rho')
+Constant(.1,'Sw')
+Constant(.1,'lw')
+Constant(.04,'le')
+Constant(0,'hw')
+Constant(-30*pi/180,'qE')
 
-initialvalues[x]=0
-initialvalues[x_d]=0
-initialvalues[y]=0
-initialvalues[y_d]=0
-initialvalues[z]=0
-initialvalues[z_d]=0
-
-statevariables = system.get_state_variables()
-ini = [initialvalues[item] for item in statevariables]
+ini = system.get_ini()
 
 Frame('N')
-Frame('A')
-Frame('B')
+#Frame('A')
+#Frame('B')
 Frame('C')
+Frame('E')
 
 system.set_newtonian(N)
-A.rotate_fixed_axis_directed(N,[1,0,0],qA,system)
-B.rotate_fixed_axis_directed(A,[0,1,0],qB,system)
-C.rotate_fixed_axis_directed(B,[0,0,1],qC,system)
+#A.rotate_fixed_axis_directed(N,[1,0,0],qA)
+#B.rotate_fixed_axis_directed(A,[0,1,0],qB)
+C.rotate_fixed_axis_directed(N,[0,0,1],qC)
+E.rotate_fixed_axis_directed(C,[0,0,1],qE)
 
-pCcm=x*N.x+y*N.y+z*N.z
+#pCcm=x*N.x+y*N.y+z*N.z
+pCcm=x*N.x+y*N.y
+pCcp=pCcm-lw*C.x+hw*C.y
+
+pC1 = pCcm
+pC2 = pCcm-l*C.x
+pE = pC2-le*E.x
 wNC = N.getw_(C)
+
+vcm = pCcm.time_derivative()
+vcp=pCcp.time_derivative()
+vcp2 = vcp.dot(vcp)
 
 IC = Dyadic.build(C,Ixx,Iyy,Izz)
 
 Body('BodyC',C,pCcm,mC,IC)
 
-system.addforcegravity(-g*N.y)
 
-points = [0*N.x,pCcm]
+vcx = vcp.dot(C.x)
+vcy = vcp.dot(C.y)
+angle_of_attack = -sympy.atan2(vcy,vcx)
+
+#cl = 2*sin(angle_of_attack)*cos(angle_of_attack)
+#cd = 2*sin(angle_of_attack)**2
+
+#fl = .5*rho*vcp2*cl*A
+#fd = .5*rho*vcp2*cd*A
+
+f_aero = rho*vcp2*sympy.sin(angle_of_attack)*Sw*C.y
+
+system.addforcegravity(-g*N.y)
+system.addforce(f_aero,vcp)
+
+points = [pC1,pC2]
 
 ang = [wNC.dot(C.x),wNC.dot(C.y),wNC.dot(C.z)]
 
@@ -91,9 +109,16 @@ f,ma = system.getdynamics()
 func1 = system.state_space_post_invert(f,ma)
 states=pynamics.integration.integrate_odeint(func1,ini,t, args=({'constants':system.constant_values},))
 
-output = Output(ang,system)
-output.calc(states)
-output.plot_time()
+#output = Output(ang,system)
+#output.calc(states)
+#output.plot_time()
 
 po = PointsOutput(points,system)
-po.calc(states)
+y=po.calc(states)
+#po.plot_time()
+#y = y.reshape((-1,2,2))
+plt.figure()
+for item in y:
+    plt.plot(*(item.T),lw=2,marker='o')
+#
+po.animate(fps = 30, movie_name='body_in_space.mp4',lw=2,marker='o')
