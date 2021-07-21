b41984c92ee17258972cfbdc13b3b108fe59cddc
Dan Aukes <danaukes@gmail.com>
2021-07-21 08:42:19 -0700

    updated init

--- python/pynamics_tests/__init__.py
+++ python/pynamics_tests/__init__.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# -*coding: utf-8 -*-
 """
 Written by Daniel M. Aukes
 Email: danaukes<at>gmail.com
@@ -6,6 +6,7 @@ Please see LICENSE for full license.
 """
 import pynamics
 pynamics.script_mode = False
+import pynamics_examples
 import pynamics_examples.babyboot
 import pynamics_examples.ball_rope
 import pynamics_examples.body_in_space
@@ -13,19 +14,24 @@ import pynamics_examples.body_in_space_local
 import pynamics_examples.bouncy2
 import pynamics_examples.bouncy_mod
 import pynamics_examples.cart_pendulum
+import pynamics_examples.cart_pendulum_forced_velocity
 import pynamics_examples.differentiating
-# import pynamics_examples.five_bar_spherical
+import pynamics_examples.falling_rod
+import pynamics_examples.five_bar_spherical
 import pynamics_examples.four_bar
 import pynamics_examples.glider
 import pynamics_examples.motor_load_side
 import pynamics_examples.motor_motor_side
-# import pynamics_examples.parallel_five_bar_jumper
-import pynamics_examples.parallel_five_bar_jumper_compliance
-# import pynamics_examples.parallel_five_bar_jumper_motor
-# import pynamics_examples.parallel_five_bar_jumper_motor_and_compliance
+import pynamics_examples.parallel_five_bar_jumper
+import pynamics_examples.parallel_five_bar_jumper_foot
 import pynamics_examples.pendulum_2_ways
-# import pynamics_examples.rolling_disc
+import pynamics_examples.pendulum_chien_wen
+import pynamics_examples.pendulum_in_water
 import pynamics_examples.single_dof_bouncer
 import pynamics_examples.springy_pendulum
+import pynamics_examples.standing_stability_test
 import pynamics_examples.triple_pendulum
-
+import pynamics_examples.triple_pendulum_fitting
+import pynamics_examples.triple_pendulum_inverse_dynamics
+import pynamics_examples.triple_pendulum_rft
+import pynamics_examples.triple_pendulum_swimmer
