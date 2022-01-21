ea30c6f7434277f31491f5df9ffce04f260895cb
dli <gdbbzq@gmail.com>
2022-01-20 18:27:35 -0700

    add plot for error bar

--- python/pynamics_examples/triangle_exp.py
+++ python/pynamics_examples/triangle_exp.py
@@ -43,6 +43,30 @@ def draw_skeleton(ini0,points1,linestyle='solid',color=[],displacement=[0,0],amp
     po2.calc(numpy.array([ini0,ini0]),[0,1])
     ax = po2.plot_time_c(newPlot=False,linestyle=linestyle,color=color,displacement=displacement,amplify=amplify)
     return ax
+def plot_one_config(angle_value,displacement=[0,0],amplify=1,side='r'):
+    initialvalues = {}   
+    initialvalues[qF]   =(angle_value)*pi/180
+    initialvalues[qF_d] =0*pi/180
+    
+    # dis_x = angle_value
+    # dis_y = max_T_value-0.03
+    
+    statevariables = system.get_state_variables()
+    ini0 = [initialvalues[item] for item in statevariables]  
+    ax2 = draw_skeleton(ini0,[pGF,pNR,pNL,pGF,pGL,pFE,pGR,pGF],linestyle='-',color='k',displacement=displacement,amplify=amplify)
+    
+    if side=='r':
+        ax2 = draw_skeleton(ini0,[pNR,pGR],linestyle='--',color='k',displacement=displacement,amplify=amplify)
+        ax2 = draw_skeleton(ini0,[pNL,pGL],linestyle='-',color='b',displacement=displacement,amplify=amplify)
+        ax2 = draw_skeleton(ini0,[pFE,p_tip_L],linestyle='-',color=[0.5,0.5,0.5],displacement=displacement,amplify=amplify)
+    if side=='l':
+        ax2 = draw_skeleton(ini0,[pNR,pGR],linestyle='-',color='r',displacement=displacement,amplify=amplify)
+        ax2 = draw_skeleton(ini0,[pNL,pGL],linestyle='--',color='k',displacement=displacement,amplify=amplify)
+        ax2 = draw_skeleton(ini0,[pFE,p_tip_R],linestyle='-',color=[0.5,0.5,0.5],displacement=displacement,amplify=amplify)
+
+    # draw_skeleton(ini0, [pGR,pV5_0],linestyle='dashed')
+    # draw_skeleton(ini0, [pGL,pV6_0],linestyle='dashed')    
+    return ax2,initialvalues
 
 
 system = System()
@@ -153,8 +177,8 @@ pV6_0 = pGL - 2*lh*u_L_FG_L
 v_R = pV5_0.time_derivative().dot(u_L_FG_R)
 v_L = pV6_0.time_derivative().dot(u_L_FG_L)
 
-l_tip_L = pGR - pFE
-l_tip_R = pGL - pFE
+l_tip_L = pGL - pFE
+l_tip_R = pGR - pFE
 
 u_l_tip_L  = (1/l_tip_L.length())*l_tip_L
 u_l_tip_R  = (1/l_tip_R.length())*l_tip_R
@@ -188,37 +212,14 @@ T_ind_sym = T_ind.subs(initialvalues).subs(system.constant_values)
 max_T_halftway = ft1_T1.subs(system.constant_values)
 
 # A_eq1 = numpy.array(max_T.jacobian(sympy.Matrix([fR,fL]))).astype(numpy.float64)
-max_fric = 1.56
+max_fric =3.0
 bounds1 = [(0,max_fric),(0,max_fric)]
 
 from scipy.optimize import minimize_scalar
 
-
-def plot_one_config(angle_value,displacement=[0,0],amplify=1,side='r'):
-    initialvalues = {}   
-    initialvalues[qF]   =(angle_value)*pi/180
-    initialvalues[qF_d] =0*pi/180
-    
-    # dis_x = angle_value
-    # dis_y = max_T_value-0.03
-    
-    statevariables = system.get_state_variables()
-    ini0 = [initialvalues[item] for item in statevariables]  
-    ax2 = draw_skeleton(ini0,[pGF,pNR,pNL,pGF,pGL,pFE,pGR,pGF],linestyle='-',color='k',displacement=displacement,amplify=amplify)
-    
-    if side=='r':
-        ax2 = draw_skeleton(ini0,[pNR,pGR],linestyle='--',color='k',displacement=displacement,amplify=amplify)
-        ax2 = draw_skeleton(ini0,[pNL,pGL],linestyle='-',color='b',displacement=displacement,amplify=amplify)
-        ax2 = draw_skeleton(ini0,[pFE,p_tip_R],linestyle='-',color=[0.5,0.5,0.5],displacement=displacement,amplify=amplify)
-    if side=='l':
-        ax2 = draw_skeleton(ini0,[pNR,pGR],linestyle='-',color='r',displacement=displacement,amplify=amplify)
-        ax2 = draw_skeleton(ini0,[pNL,pGL],linestyle='--',color='k',displacement=displacement,amplify=amplify)
-        ax2 = draw_skeleton(ini0,[pFE,p_tip_L],linestyle='-',color=[0.5,0.5,0.5],displacement=displacement,amplify=amplify)
-
-    # draw_skeleton(ini0, [pGR,pV5_0],linestyle='dashed')
-    # draw_skeleton(ini0, [pGL,pV6_0],linestyle='dashed')    
-    return ax2,initialvalues
-
+fig,ax2 = plt.subplots(111)
+# ax2.set_xlim(([-60,60]))
+fig3,ax3 = plt.subplots(111)
 
 angle_start = -45
 angle_end = 45
@@ -226,6 +227,18 @@ num = 30
 plt.close('all')
 t_max1 = []
 t_max2 = []
+
+from scipy import interpolate
+t_forces1 = numpy.genfromtxt('triangle_force_exp_forplot.csv',delimiter=',')
+t_forces = t_forces1*-0.035
+# t_force_temp = t_forces[:,0]
+t_force_temp_max = numpy.amax(t_forces,axis=0)
+t_force_temp_min = numpy.amin(t_forces,axis=0)
+exp_angles = numpy.linspace(angle_start,angle_end,7)
+ft_max = interpolate.interp1d(exp_angles,t_force_temp_max,fill_value = 'extrapolate', kind='quadratic')
+ft_min = interpolate.interp1d(exp_angles,t_force_temp_min,fill_value = 'extrapolate', kind='quadratic')
+
+
 for item in numpy.linspace(angle_start,angle_end,num):
     initialvalues = {}   
     initialvalues[qF]   =(item)*pi/180
@@ -234,25 +247,29 @@ for item in numpy.linspace(angle_start,angle_end,num):
     max_T = max_T_halftway.subs(initialvalues)
     obj1=lambda f_input:(max_T.subs({fR:f_input[0],fL:f_input[1]}))[0]
     obj2=lambda f_input:(-max_T.subs({fR:f_input[0],fL:f_input[1]}))[0]
-    res1 = minimize(obj1,[0,0],bounds=bounds1,options={'disp':False})
-    res2 = minimize(obj2,[0,0],bounds=bounds1,options={'disp':False})    
-    # max_T_value1 = -(max_T.subs({fR:0,fL:3.2}))[0]
-    # max_T_value2 = (max_T.subs({fR:3.2,fL:0}))[0]
-    t_max1 = numpy.append(t_max1,res1.fun)
-    t_max2 = numpy.append(t_max2,res2.fun)
+    # res1 = minimize(obj1,[0,0],bounds=bounds1,options={'disp':False})
+    # res2 = minimize(obj2,[0,0],bounds=bounds1,options={'disp':False})    
+    max_T_value1 = -(max_T.subs({fR:0,fL:3.2}))[0]
+    max_T_value2 = (max_T.subs({fR:3.2,fL:0}))[0]
+    t_max1 = numpy.append(t_max1,max_T_value1)
+    t_max2 = numpy.append(t_max2,max_T_value2)
 
-    
 # fig, ax = plt.subplots()
 plt.plot(numpy.linspace(angle_start,angle_end,num),t_max1*1000,'b')
 plt.plot(numpy.linspace(angle_start,angle_end,num),t_max2*1000,'r')
 plt.ylabel("Max Torque")
 plt.xlabel("Joint angle")
-plt.ylim([5,51])
+# plt.ylim([5,51])
 plt.show()
+
+sim_angles = numpy.linspace(angle_start,angle_end,num)
+plt.fill_between(sim_angles,ft_max(sim_angles)*1000,ft_min(sim_angles)*1000,color='b',alpha=0.25)
+plt.fill_between(sim_angles,ft_max(numpy.flip(sim_angles))*1000,ft_min(numpy.flip(sim_angles))*1000,color='r',alpha=0.25)
+
 
 
 T_loc = (t_max1+t_max2)/2
-from scipy import interpolate
+
 ft0 = interpolate.interp1d(numpy.linspace(angle_start,angle_end,num),T_loc,fill_value = 'extrapolate', kind='quadratic')
 ft1 = interpolate.interp1d(numpy.linspace(angle_start,angle_end,num),t_max1,fill_value = 'extrapolate', kind='quadratic')
 
@@ -267,67 +284,70 @@ for item in numpy.linspace(angle_start,angle_end,7):
     # max_T_value = -obj1(res.x)
    
     dis_x = item
-    dis_y = ft0(item)
-    plot_one_config(item,displacement=[dis_x,dis_y*1000-20],amplify=100,side='l') 
+    dis_y = ft0(angle_start+angle_end)/2
+    plot_one_config(item,displacement=[dis_x,dis_y*1000-100],amplify=100,side='l') 
     
     error_string1 = "%.7f" % (ft1(item)/0.035)
     plt.text(dis_x,dis_y*1000,error_string1,ha='center',va='top') 
     
-    # plot_one_config(item,displacement=[dis_x,dis_y*1000-20],amplify=100,ax=[]) 
+    # plot_one_config(item,displacement=[dis_x,dis_y*1000-0],amplify=100,ax=[]) 
     # t_max = numpy.append(t_max,max_T_value)
 
-for item in numpy.linspace(angle_start,angle_end,5):
+for item in numpy.linspace(angle_start,angle_end,7):
     initialvalues = {}   
     initialvalues[qF]   =(item)*pi/180
     initialvalues[qF_d] =0*pi/180
     dis_x = item
-    dis_y = ft0(item)    
-    plot_one_config(item,displacement=[dis_x,dis_y*1000+10],amplify=100,side='r') 
+    dis_y = ft1(item)/2    
+    plot_one_config(item,displacement=[dis_x,dis_y*1000+20],amplify=100,side='r') 
     # error_string1 = "%.7f" % (dis_y/0.035)
     # plt.text(dis_x,dis_y*1000+60,error_string1,ha='center',va='top') 
     # t_max = numpy.append(t_max,max_T_value)
 
-def calculate_f_dump(x1):
-    value3 = numpy.sum(numpy.asanyarray(x1)**2)*1
-    return value3
+plt.xlim([-60,60])
+plt.show()
+plt.xticks(exp_angles)
+# def calculate_f_dump(x1):
+#     value3 = numpy.sum(numpy.asanyarray(x1)**2)*1
+#     return value3
 
-# bounds1 = [(-1e3,1e3),(-1e3,1e3)]
-def calculate_force_angle(load):
+# # bounds1 = [(-1e3,1e3),(-1e3,1e3)]
+# def calculate_force_angle(load):
 
-    cond1 = {}
-    # cond1[lA] = l_d1
-    # cond1[lh] = l_t_h
-    # cond1[lT] = l_t_w
-    Fx_tip = sympy.Symbol('Fx_tip')
-    Fy_tip= sympy.Symbol('Fy_tip')
-    T_tip= sympy.Symbol('T_tip')
-    cond1[Fx_tip] = load[0]
-    cond1[Fy_tip] = load[1]
-    cond1[T_tip] = load[2]
-    tol=1e-4
-    ft_error_sym_T1 = ft_error_T1.subs(cond1)
-    T_ind_value = T_ind_sym.subs(cond1)
-    # print(ft_error_sym_T1)
-    # bounds1 = [(-1e3,1e3),(-1e3,1e3)]    
-    bounds1 = [(0,1e3),(0,1e3)]
-    # bounds1 = [(-1e3,0)]
+#     cond1 = {}
+#     # cond1[lA] = l_d1
+#     # cond1[lh] = l_t_h
+#     # cond1[lT] = l_t_w
+#     Fx_tip = sympy.Symbol('Fx_tip')
+#     Fy_tip= sympy.Symbol('Fy_tip')
+#     T_tip= sympy.Symbol('T_tip')
+#     cond1[Fx_tip] = load[0]
+#     cond1[Fy_tip] = load[1]
+#     cond1[T_tip] = load[2]
+#     tol=1e-4
+#     ft_error_sym_T1 = ft_error_T1.subs(cond1)
+#     T_ind_value = T_ind_sym.subs(cond1)
+#     # print(ft_error_sym_T1)
+#     # bounds1 = [(-1e3,1e3),(-1e3,1e3)]    
+#     bounds1 = [(0,1e3),(0,1e3)]
+#     # bounds1 = [(-1e3,0)]
     
-    A_eq  = numpy.array(ft_error_sym_T1.jacobian(sympy.Matrix([fR,fL]))).astype(numpy.float64)
-    lb1 = -numpy.array(ft_error_sym_T1.subs({fR:0,fL:0})).astype(numpy.float64)
-    ub1 = -numpy.array(ft_error_sym_T1.subs({fR:0,fL:0})).astype(numpy.float64)
-    lb = numpy.transpose(lb1).reshape(1) - tol
-    ub = numpy.transpose(ub1).reshape(1) + tol
-    con1 = LinearConstraint(A_eq, lb, ub)
+#     A_eq  = numpy.array(ft_error_sym_T1.jacobian(sympy.Matrix([fR,fL]))).astype(numpy.float64)
+#     lb1 = -numpy.array(ft_error_sym_T1.subs({fR:0,fL:0})).astype(numpy.float64)
+#     ub1 = -numpy.array(ft_error_sym_T1.subs({fR:0,fL:0})).astype(numpy.float64)
+#     lb = numpy.transpose(lb1).reshape(1) - tol
+#     ub = numpy.transpose(ub1).reshape(1) + tol
+#     con1 = LinearConstraint(A_eq, lb, ub)
     
-    res = minimize(calculate_f_dump,[0,0],bounds=bounds1,constraints=con1,options={'disp':True})
+#     res = minimize(calculate_f_dump,[0,0],bounds=bounds1,constraints=con1,options={'disp':True})
     
-    # max_T_value = max_T.subs({fR:1,fL:1})
+#     # max_T_value = max_T.subs({fR:1,fL:1})
     
-    # print("T_ind value1")
-    # print(T_ind.subs(initialvalues).subs(cond1))
-    # print("T_ind value2")
-    # print(((J_t_ind_T1.T).subs(initialvalues).subs(cond1)).dot(res.x))
-    return [res.x,ft_error_sym_T1,T_ind_value]
+#     # print("T_ind value1")
+#     # print(T_ind.subs(initialvalues).subs(cond1))
+#     # print("T_ind value2")
+#     # print(((J_t_ind_T1.T).subs(initialvalues).subs(cond1)).dot(res.x))
+#     return [res.x,ft_error_sym_T1,T_ind_value]
 
 # para = [1*pi/3,0,pi/6,pi/6,pi/6,0.01,0.06,0.1,2]   
 
